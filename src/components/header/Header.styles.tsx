13e350f788d05593dbc24c888cc90c38616eae1f
Nathalia Duarte <duartenatha@gmail.com>
2022-05-11 16:16:46 -0300

    started styling header

--- src/components/header/Header.styles.tsx
+++ src/components/header/Header.styles.tsx
@@ -1,10 +1,15 @@
 import styled from "styled-components";
 
 export const StyledNav = styled.nav`
+
 `;
 export const ListNav = styled.ul`
   list-style: none;
   margin: 0;
+  display: flex;
+  gap: 20px;
+  align-items: center;
+  justify-content: center;
 `;
 export const ItemNav = styled.li``;
 export const StyledHeader = styled.div`
